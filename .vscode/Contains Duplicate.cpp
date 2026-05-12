120852f3e0bfd7f68a4348d32050d4cdc9134a61
Tanish Dua <tanishdua.30@gmail.com>
2026-05-12 18:28:13 +0000

    Day 1: Ques4 Contains Duplicate

---
+++ .vscode/Contains Duplicate.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // Use a Hash Set for O(1) average time complexity lookups
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            // If num is already in the set, we found a duplicate
+            if (seen.find(num) != seen.end()) {
+                return true;
+            }
+            // Add current number to the set
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
+
+// Driver code for local testing
+int main() {
+    Solution sol;
+    vector<int> nums = {1, 2, 3, 1};
+    
+    if (sol.containsDuplicate(nums)) {
+        cout << "Duplicate Found!" << endl;
+    } else {
+        cout << "No Duplicates." << endl;
+    }
+    
+    return 0;
+}
